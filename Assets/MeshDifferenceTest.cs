d7caff0004f0d072c4dff426669ec5e4a1eeca48
maxmills372 <topmanmax@gmail.com>
2018-09-19 18:17:29 +0100

    made PS better

---
+++ Assets/MeshDifferenceTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDifferenceTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Mesh m = col.GetComponent<MeshFilter>().mesh;
+		Collider[] c = new Collider[1];
+		for (int i = 0; i < m.vertices.Length; i++) 
+		{
+			
+		}
+
+	}		
+
+}
+
